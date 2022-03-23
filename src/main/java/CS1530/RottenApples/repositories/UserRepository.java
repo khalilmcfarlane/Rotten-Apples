35ba9d1257893fa8f364830f7e286f1a606c339c
Khalil McFarlane <kmac36960@gmail.com>
2022-03-23 15:49:08 -0400

    Made login and register helper methods

--- src/main/java/CS1530/RottenApples/repositories/UserRepository.java
+++ src/main/java/CS1530/RottenApples/repositories/UserRepository.java
@@ -2,11 +2,13 @@ package CS1530.RottenApples.repositories;
 
 import java.util.*;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import CS1530.RottenApples.models.User;
 
 @Repository
 public interface UserRepository extends MongoRepository<User, UUID> { 
-    
+    @Query("{'username' : ?0, 'password' : ?1}")
+    User findByUser(String username, String password);
 }
