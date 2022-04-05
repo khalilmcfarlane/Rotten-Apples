09fedc3328167da8fe2850192473ca6eb67a6969
Khalil McFarlane <kmac36960@gmail.com>
2022-04-05 13:49:00 -0400

    Created a get friends method

--- src/main/java/CS1530/RottenApples/impl/FriendImpl.java
+++ src/main/java/CS1530/RottenApples/impl/FriendImpl.java
@@ -1,13 +1,21 @@
 package CS1530.RottenApples.impl;
 
+import java.util.List;
+
 import CS1530.RottenApples.models.*;
 import CS1530.RottenApples.repositories.FriendRequestRepository;
 import CS1530.RottenApples.repositories.UserRepository;
+import CS1530.RottenApples.repositories.friendRequestRepository;
        
 public class FriendImpl {
     private FriendRequestRepository friendRequestRepository;
     private UserRepository userRepository;
 
+    public List<friendRequest> getFriends(User user) {
+        List<friendRequest> friendList = friendRequestRepository.findUser(user.getUsername());
+        return friendList;
+    }
+    
     public void addToFriends(String currentUser, String friendUserName) {
         User curr = userRepository.findFriend(currentUser);
         User friend = userRepository.findFriend(friendUserName);
--- src/main/java/CS1530/RottenApples/repositories/friendRequestRepository.java
+++ src/main/java/CS1530/RottenApples/repositories/friendRequestRepository.java
@@ -1,6 +1,9 @@
 package CS1530.RottenApples.repositories;
 import CS1530.RottenApples.models.friendRequest;
 import CS1530.RottenApples.models.User;
+
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
@@ -9,4 +12,7 @@ import org.springframework.stereotype.Repository;
 public interface FriendRequestRepository extends MongoRepository<friendRequest, Long> {
     @Query("{'recipient' : ?0, 'sender' : ?1}")
     friendRequest findFriend(User recipient, User sender);
+
+    @Query("{'user' : ?0}")
+    List<friendRequest> findUser(String username);
 }
