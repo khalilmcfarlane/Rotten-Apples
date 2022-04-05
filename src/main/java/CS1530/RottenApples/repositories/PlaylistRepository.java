2700dad0ecf325139682d542dac3a33ac1aff878
Janet Majekodunmi <55996178+joyousjay@users.noreply.github.com>
2022-04-05 14:37:04 -0400

    updated code to pull from Playlist model

--- src/main/java/CS1530/RottenApples/repositories/PlaylistRepository.java
+++ src/main/java/CS1530/RottenApples/repositories/PlaylistRepository.java
@@ -5,11 +5,11 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
-import CS1530.RottenApples.models.User;
+import CS1530.RottenApples.models.Playlist;
 
 
-//Waiting for playlist model for query and searchable playlist
-// @Repository
-// public interface PlaylistRepository extends MongoRepository<Playlist, Integer> { 
 
-// }
+ @Repository
+ public interface PlaylistRepository extends MongoRepository<Playlist, Integer> { 
+    Playlist findbyPlaylist(String playlistTitle);
+ }
