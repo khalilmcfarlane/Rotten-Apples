d533521d0fa5f55332f28e7e907acbd1251f4870
Ari Seymour <ars311@pitt.edu>
2022-04-20 11:05:37 -0700

    Added a line to login func

--- src/main/java/CS1530/RottenApples/controllers/UserController.java
+++ src/main/java/CS1530/RottenApples/controllers/UserController.java
@@ -1,61 +1,56 @@
 package CS1530.RottenApples.controllers;
 
 import java.util.List;
-import java.util.UUID;
 
 import javax.servlet.http.HttpSession;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
 import CS1530.RottenApples.impl.FriendImpl;
 import CS1530.RottenApples.impl.UserImpl;
-import CS1530.RottenApples.models.Playlist;
 import CS1530.RottenApples.models.User;
 import CS1530.RottenApples.models.friendRequest;
-import CS1530.RottenApples.repositories.PlaylistRepository;
 import CS1530.RottenApples.repositories.UserRepository;
-//import friendRequest and friendRepository
 
-@RestController
-public class UserController {
+@Controller
+public class userController {
+    @Autowired
     private UserRepository userRepository;
-    private PlaylistRepository playlistRepository;
 
-    UserImpl newUser, loginUser;
+    private UserImpl newUser;
     
-    //I think this code goes inside homePage.java
     @PostMapping("/signup")
-    public String register(@RequestParam("username") String username, String password, BindingResult bindingResult) {
+    public String register(String username, String password, BindingResult bindingResult) {
         newUser = new UserImpl();
         User user = userRepository.findByName(username);
         if(user != null) {
-            bindingResult.rejectValue("username", "error.user", "This username already exists.");
-            return "redirect:/signup";
+            bindingResult.rejectValue("username", "error.user", "This user already exists.");
+            return "signup";
         }  
         if(bindingResult.hasErrors()) {
             return "signup";
         }
         newUser.register(username, password);
-        //After user has registered, how to automatically login in?  
-        //Where is the user redirected?
         return "signup";
     }
 
     @PostMapping("/login")
     public String login(String username, String password, HttpSession session) {
-        loginUser = new UserImpl();
+        UserImpl loginUser = new UserImpl();
         User user = userRepository.findByUser(username, password);
         if(user == null) {
             return "redirect:/UserLogin?error";
         }
         else {
             session.setAttribute("user", user);
+            //Is this needed?
+            loginUser.login(username, password);
             //What do I return to?
             return "UserLogin";
         } 
@@ -69,7 +64,7 @@ public class UserController {
     }
 
     //Need to map to an HTML page
-    //@GetMapping("{username}/friends")
+    @GetMapping("{username}/friends")
     public void displayFriends(Model model, @PathVariable String username) {
         FriendImpl friend = new FriendImpl(); 
         User user = userRepository.findByName(username);
