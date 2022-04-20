67546a022e3bdd8e55395f0148b28d7d74dee87c
Ari Seymour <ars311@pitt.edu>
2022-04-19 20:39:05 -0700

    Error w/registration

--- src/main/java/CS1530/RottenApples/controllers/UserController.java
+++ src/main/java/CS1530/RottenApples/controllers/UserController.java
@@ -1,54 +1,83 @@
-package CS1530.controllers;
+package CS1530.RottenApples.controllers;
 
 import java.util.List;
 import java.util.UUID;
 
+import javax.servlet.http.HttpSession;
+
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import CS1530.RottenApples.impl.FriendImpl;
+import CS1530.RottenApples.impl.UserImpl;
 import CS1530.RottenApples.models.Playlist;
 import CS1530.RottenApples.models.User;
 import CS1530.RottenApples.models.friendRequest;
-import CS1530.RottenApples.repositories.FriendRequestRepository;
+import CS1530.RottenApples.repositories.PlaylistRepository;
 import CS1530.RottenApples.repositories.UserRepository;
 //import friendRequest and friendRepository
 
 @RestController
 public class UserController {
     private UserRepository userRepository;
-    //private FriendRequestRepository friendRepository;
+    private PlaylistRepository playlistRepository;
 
-    public void register(String username, String password) {
-        userRepository.save(new User(UUID.randomUUID(), username, password));
+    UserImpl newUser, loginUser;
+    
+    //I think this code goes inside homePage.java
+    @PostMapping("/signup")
+    public String register(@RequestParam("username") String username, String password, BindingResult bindingResult) {
+        newUser = new UserImpl();
+        User user = userRepository.findByName(username);
+        if(user != null) {
+            bindingResult.rejectValue("username", "error.user", "This username already exists.");
+            return "redirect:/signup";
+        }  
+        if(bindingResult.hasErrors()) {
+            return "signup";
+        }
+        newUser.register(username, password);
+        //After user has registered, how to automatically login in?  
+        //Where is the user redirected?
+        return "signup";
     }
 
-    public boolean login(String username, String password) {
+    @PostMapping("/login")
+    public String login(String username, String password, HttpSession session) {
+        loginUser = new UserImpl();
         User user = userRepository.findByUser(username, password);
-
-        if(user.getUsername() != null && user.getPassword() != null) {
-            return true;
+        if(user == null) {
+            return "redirect:/UserLogin?error";
         }
-        return false;
+        else {
+            session.setAttribute("user", user);
+            //What do I return to?
+            return "UserLogin";
+        } 
     }
 
-    public void signout() {
+    @GetMapping(value = "logout")
+    public String logout(HttpSession session) {
         //skeleton
+        session.invalidate();
+        return "redirect:/login";
     }
 
-    public void displayFriends(User user) {
+    //Need to map to an HTML page
+    //@GetMapping("{username}/friends")
+    public void displayFriends(Model model, @PathVariable String username) {
         FriendImpl friend = new FriendImpl(); 
+        User user = userRepository.findByName(username);
         List<friendRequest> friends = friend.getFriends(user); //get the friends of this user
-        for(friendRequest reqs : friends) {
+        model.addAttribute("Friends", friends);
+        /*for(friendRequest reqs : friends) {
+            //display users
             System.out.print(reqs.toString().replaceAll("[]", ""));
-        }
-    }
-     
-    public void createPlaylist() {
-        //skeleton
-        Playlist user = new Playlist();
-        user.setPlaylistTitle("How can I make this generic?");
-        //user.addMovieToPlaylist();
-        //user.addFriendToPlaylist();
-        //blah, blah
+        }*/
     }
 }
